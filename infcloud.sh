18d6a77a08c39202d6ddb3aff8e242b96387963c
Jan Wagner <waja@cyconet.org>
2016-06-23 23:04:19 +0200

    Remove unneeded code

--- infcloud.sh
+++ infcloud.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env ash
 [ ! -f /srv/infcloud/config/config.js ] && cp /srv/infcloud/config.js.orig /srv/infcloud/config/config.js; \
-#    chown -R lighttpd. /srv/infcloud && \
     lighttpd -D -f /etc/lighttpd/lighttpd.conf
 
