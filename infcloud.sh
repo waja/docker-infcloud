037ab318d2bede204269870cea0b59d19b8a71e6
Jan Wagner <waja@cyconet.org>
2016-06-28 22:43:46 +0200

    Update cache when starting container

--- infcloud.sh
+++ infcloud.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env ash
 [ ! -f /srv/infcloud/config/config.js ] && cp /srv/infcloud/config.js.orig /srv/infcloud/config/config.js; \
+/srv/infcloud/cache_update.sh; \
 [ ! -f /etc/lighttpd/lighttpd.conf ] && cp /tmp/lighttpd.conf /etc/lighttpd/lighttpd.conf; \
     lighttpd -D -f /etc/lighttpd/lighttpd.conf
 
