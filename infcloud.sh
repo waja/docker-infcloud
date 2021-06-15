7e079f2bc227588d6e3ad8fa25996ec4819c1317
Jan Wagner <waja@cyconet.org>
2021-06-15 15:10:32 +0200

    Silnce SC2187

--- infcloud.sh
+++ infcloud.sh
@@ -1,3 +1,4 @@
+# shellcheck shell=dash
 #!/usr/bin/env ash
 [ ! -f /srv/infcloud/config/config.js ] && cp /srv/infcloud/config.js.orig /srv/infcloud/config/config.js; \
 /srv/infcloud/cache_update.sh; \
